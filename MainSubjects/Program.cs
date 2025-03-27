7e8eb169f33a6681b5741d7b2cb0d2d64e28c3fa
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2025-03-27 21:29:26 +0300

    terminalden yukleme

---
+++ MainSubjects/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// usingler yuklenen kutuphaneleri gosterir
+// namespace projeye verilen isim
+namespace MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args) // main methodu
+        {
+            #region Yazdirma komutlari
+            Console.Write("Merhaba Dünya"); // method oldugunu solunda cıkan mor kupten anlarsin
+            Console.Write("selam");
+            Console.Read();
+            #endregion 
+            // bu regionlari kapatabiliyorusn sagdaki ok ile.
+            int deneme = 5;
+
+        }
+    }
+}
